b6e7c23a791a6eaae0903afe98ea3441ac358b62
sidsupar <38404208+sidsupar@users.noreply.github.com>
2024-01-02 16:02:47 +0530

    Updating user.js

--- middleware/user.js
+++ middleware/user.js
@@ -5,7 +5,7 @@ const secretKey = process.env.secretKey;
 
 function userMiddleware(req, res, next) {
     // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    // You need to check the headers and validate the user from the user DB.
     const tokenReceived = req.headers.authorization.split(" ")[1].trim();
     try{
         jwt.verify(tokenReceived, secretKey);
